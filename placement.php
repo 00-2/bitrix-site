06dbac9fb7e360f0adaf67d9d3dc5d68f1729f20
MaksimKaraganov <mkaraganov_work@mail.ru>
2025-07-27 11:21:06 +0000

    dismiss old functional

--- placement.php
+++ placement.php
@@ -1,26 +1,16 @@
 <?php
 require_once (__DIR__.'/crest.php');
-
-function displayValue($value) {
-	if (is_array($value)) {
-		$result = '';
-		foreach ($value as $item) $result .= $item.', ';
-		return $result;
-	} else return $value;
-}
 ?>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-
 	<link rel="stylesheet" href="css/app.css">
 	<script src="https://code.jquery.com/jquery-3.6.0.js"
 		integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
 		crossorigin="anonymous"></script>
-
-	<title>Placement</title>
+	<title>Deal View</title>
 	<style>
 		iframe {
 			width: 100%;
@@ -32,7 +22,6 @@ function displayValue($value) {
 <body class="container-fluid">
 
 <?php
-// вывод debug
 echo '<div class="alert alert-success" role="alert"><pre>';
 print_r($_REQUEST);
 echo '</pre></div>';
@@ -40,13 +29,13 @@ echo '</pre></div>';
 // Получение ID сделки из PLACEMENT_OPTIONS
 $placement_options = json_decode($_REQUEST['PLACEMENT_OPTIONS'] ?? '', true);
 //$deal_id = $placement_options['ID'] ?? null;
-$deal_id = 22125;
-if ($deal_id):
-	$url = "http://51.250.13.43:5000/order_card?order_id={$deal_id}";
-	echo "<iframe src=\"{$url}\"></iframe>";
-else:
-	echo "<div class=\"alert alert-danger\">Не удалось определить ID сделки</div>";
-endif;
+$deal_id = 22105;
+if ($deal_id) {
+	$url = "http://51.250.13.43:5000/order_card?order_id=" . urlencode($deal_id);
+	echo "<iframe src=\"$url\"></iframe>";
+} else {
+	echo "<div class='alert alert-danger'>Ошибка: ID сделки не найден.</div>";
+}
 ?>
 
 </body>
