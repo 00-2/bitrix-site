8120e31e8e3d7ec8875671cc1e5f82742d38d0b7
MaksimKaraganov <mkaraganov_work@mail.ru>
2025-07-27 10:57:48 +0000

    test changing

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 <body class="container-fluid box background-tint">
 <div class="center-block">
 	<h1>How to use our app?</h1>
-	<h2>Step 1</h2>
+	<h2>Step 5</h2>
 	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 	<h2>Step 2</h2>
 	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum./p>
