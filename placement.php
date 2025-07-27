153fa4fb5819ed874f5a61b5530b8c69eeef4f40
MaksimKaraganov <mkaraganov_work@mail.ru>
2025-07-27 10:49:17 +0000

    Open web-wie on start panel

--- placement.php
+++ placement.php
@@ -1,4 +1,4 @@
-<?
+<?php
 require_once (__DIR__.'/crest.php');
 
 function displayValue($value) {
@@ -6,10 +6,8 @@ function displayValue($value) {
 		$result = '';
 		foreach ($value as $item) $result .= $item.', ';
 		return $result;
-
 	} else return $value;
 }
-
 ?>
 <html>
 <head>
@@ -17,42 +15,39 @@ function displayValue($value) {
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="css/app.css">
-	<!--link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"-->
-	<script
-		src="https://code.jquery.com/jquery-3.6.0.js"
+	<script src="https://code.jquery.com/jquery-3.6.0.js"
 		integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
 		crossorigin="anonymous"></script>
 
 	<title>Placement</title>
+	<style>
+		iframe {
+			width: 100%;
+			height: 95vh;
+			border: none;
+		}
+	</style>
 </head>
 <body class="container-fluid">
-<div class="alert alert-success" role="alert"><pre>
-	<?
-	print_r($_REQUEST);
-	?>
-	</pre>
-</div>
-<?
-$placement_options = json_decode($_REQUEST['PLACEMENT_OPTIONS'], true);
 
-$deal = CRest::call(
-	'crm.deal.get',
-	[
-		'ID' => $placement_options['ID']
-	]
-);
+<?php
+// вывод debug
+echo '<div class="alert alert-success" role="alert"><pre>';
+print_r($_REQUEST);
+echo '</pre></div>';
 
-if ($deal['error'] == ''):
-	?>
-	<table class="table table-striped">
-		<?foreach ($deal['result'] as $field => $value):?>
-			<tr>
-				<td><?=$field;?></td>
-				<td><?=displayValue($value);?></td>
-			</tr>
-		<?endforeach;?>
-	</table>
-<?endif;?>
+// Получение ID сделки из PLACEMENT_OPTIONS
+$placement_options = json_decode($_REQUEST['PLACEMENT_OPTIONS'] ?? '', true);
+//$deal_id = $placement_options['ID'] ?? null;
+$deal_id = 22125
+if ($deal_id):
+	$url = "http://51.250.13.43:5000/order_card?order_id={$deal_id}";
+	echo "<iframe src=\"{$url}\"></iframe>";
+else:
+	echo "<div class=\"alert alert-danger\">Не удалось определить ID сделки</div>";
+endif;
+?>
+
 </body>
+</html>
