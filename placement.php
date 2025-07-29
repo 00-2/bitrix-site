62a8f99c84e151d88de1c7a218fd38f7b4157c34
MaksimKaraganov <mkaraganov_work@mail.ru>
2025-07-29 16:21:28 +0000

    Copy index to placment

--- placement.php
+++ placement.php
@@ -1,58 +1,38 @@
-<?
-require_once (__DIR__.'/crest.php');
-
-function displayValue($value) {
-	if (is_array($value)) {
-		$result = '';
-		foreach ($value as $item) $result .= $item.', ';
-		return $result;
-
-	} else return $value;
-}
-
+<?php
+$order_id = 22208;
+$exec_vk_id = 305852380;
+$url = "https://startproj.ru/order_card/formal?order_id=$order_id&exec_vk_id=$exec_vk_id";
 ?>
+<!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="css/app.css">
-	<!--link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"-->
 	<script
 		src="https://code.jquery.com/jquery-3.6.0.js"
 		integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
 		crossorigin="anonymous"></script>
 
-	<title>Placement</title>
+	<style>
+		body, html {
+			margin: 0;
+			padding: 0;
+			height: 100%;
+			background-color: #f5f5f5;
+		}
+		iframe {
+			border: none;
+			width: 100%;
+			height: 100vh;
+		}
+	</style>
+
+	<title>Order Card Viewer</title>
 </head>
-<body class="container-fluid">
-<div class="alert alert-success" role="alert"><pre>
-	<?
-	print_r($_REQUEST);
-	?>
-	</pre>
-</div>
-<?
-$placement_options = json_decode($_REQUEST['PLACEMENT_OPTIONS'], true);
-
-$deal = CRest::call(
-	'crm.deal.get',
-	[
-		'ID' => $placement_options['ID']
-	]
-);
-
-if ($deal['error'] == ''):
-	?>
-	<table class="table table-striped">
-		<?foreach ($deal['result'] as $field => $value):?>
-			<tr>
-				<td><?=$field;?></td>
-				<td><?=displayValue($value);?></td>
-			</tr>
-		<?endforeach;?>
-	</table>
-<?endif;?>
+<body>
+	<iframe src="<?php echo htmlspecialchars($url); ?>"></iframe>
 </body>
+</html>
