b1b2c6355b9fdd696cc24e19d5dfe994240f386a
MaksimKaraganov <mkaraganov_work@mail.ru>
2025-07-29 17:37:22 +0000

    Debug

--- TestApp.php
+++ TestApp.php
@@ -12,29 +12,7 @@ function displayValue($value) {
 
 ?>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="css/app.css">
-	<!--link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"-->
-	<script
-		src="https://code.jquery.com/jquery-3.6.0.js"
-		integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-		crossorigin="anonymous"></script>
-
-	<title>Placement</title>
-</head>
-<body class="container-fluid">
-<div class="alert alert-success" role="alert"><pre>
-	<?
-	print_r($_REQUEST);
-	?>
-	</pre>
-</div>
-<?
+<?php
 $placement_options = json_decode($_REQUEST['PLACEMENT_OPTIONS'], true);
 
 $deal = CRest::call(
@@ -43,13 +21,13 @@ $deal = CRest::call(
 		'ID' => $placement_options['ID']
 	]
 );
-
-if ($deal['error'] == ''):
+;?>
+<body class="container-fluid">
+<div class="alert alert-success" role="alert"><pre>
+	<?
+	displayValue($value);
 	?>
-	<table class="table table-striped">
-		<tr>
-			<td><?=$deal ?></td>//displayValue($value);
-		</tr>
-	</table>
-<?endif;?>
+	</pre>
+</div>
+
 </body>
