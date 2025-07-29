df2a83c7ccb97c43650a0df4090b20491363ce8e
MaksimKaraganov <mkaraganov_work@mail.ru>
2025-07-29 17:45:42 +0000

    debug

--- TestApp.php
+++ TestApp.php
@@ -1,33 +1,9 @@
-<?php
-require_once (__DIR__.'/crest.php');
-
-function displayValue($value) {
-	if (is_array($value)) {
-		$result = '';
-		foreach ($value as $item) $result .= $item.', ';
-		return $result;
-
-	} else return $value;
-}
-
-?>
 <html>
-<?php
-$placement_options = json_decode($_REQUEST['PLACEMENT_OPTIONS'], true);
-
-$deal = CRest::call(
-	'crm.deal.get',
-	[
-		'ID' => $placement_options['ID']
-	]
-);
-;?>
 <body class="container-fluid">
 <div class="alert alert-success" role="alert"><pre>
 	<?
-	displayValue($value);
+	print_r($_REQUEST);
 	?>
 	</pre>
 </div>
-
 </body>
