a0a5b67fb86a9c958d4aa1cabe44244a47805613
MaksimKaraganov <mkaraganov_work@mail.ru>
2025-07-29 17:51:42 +0000

    debug

--- TestApp.php
+++ TestApp.php
@@ -2,7 +2,7 @@
 <body class="container-fluid">
 <div class="alert alert-success" role="alert"><pre>
 	<?
-	print_r($_REQUEST);
+	print_r(json_decode($_REQUEST['PLACEMENT_OPTIONS'], true));
 	?>
 	</pre>
 </div>
